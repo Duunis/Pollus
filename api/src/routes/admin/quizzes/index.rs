4afd7c398c1f1fc34a4bd90ef538515e5c22a8d3
Janne Vanhanen <janne.vanhanen@outlook.com>
2022-10-30 19:51:44 +0200

    Return 401 for missing admin key

--- api/src/routes/admin/quizzes/index.rs
+++ api/src/routes/admin/quizzes/index.rs
@@ -5,34 +5,34 @@ use crate::RequestData;
 pub async fn post(_req: Request, ctx: RouteContext<RequestData>) -> Result<Response> {
     match ctx.data.admin_key {
         Some(admin_key) => Response::ok(admin_key),
-        None => Response::error("Admin key is missing from the request", 400)
+        None => Response::error("Admin key is missing from the request", 401)
     }
 }
 
 pub async fn get_id(_req: Request, ctx: RouteContext<RequestData>) -> Result<Response> {
     match ctx.data.admin_key {
         Some(admin_key) => Response::ok(admin_key),
-        None => Response::error("Admin key is missing from the request", 400)
+        None => Response::error("Admin key is missing from the request", 401)
     }
 }
 
 pub async fn post_id(_req: Request, ctx: RouteContext<RequestData>) -> Result<Response> {
     match ctx.data.admin_key {
         Some(admin_key) => Response::ok(admin_key),
-        None => Response::error("Admin key is missing from the request", 400)
+        None => Response::error("Admin key is missing from the request", 401)
     }
 }
 
 pub async fn put_id(_req: Request, ctx: RouteContext<RequestData>) -> Result<Response> {
     match ctx.data.admin_key {
         Some(admin_key) => Response::ok(admin_key),
-        None => Response::error("Admin key is missing from the request", 400)
+        None => Response::error("Admin key is missing from the request", 401)
     }
 }
 
 pub async fn delete_id(_req: Request, ctx: RouteContext<RequestData>) -> Result<Response> {
     match ctx.data.admin_key {
         Some(admin_key) => Response::ok(admin_key),
-        None => Response::error("Admin key is missing from the request", 400)
+        None => Response::error("Admin key is missing from the request", 401)
     }
 }
