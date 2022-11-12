8a9ff7c7d79091eb690238156bdfacbeca8f1dd9
Janne Vanhanen <janne.vanhanen@outlook.com>
2022-11-12 18:04:00 +0200

    TUN KTUN TKUN

--- functions/sentry.ts
+++ functions/sentry.ts
@@ -4,7 +4,7 @@ type Context = Parameters<Handler>[0]
 
 export const sentry = (handler: Handler) => (ctx: Context) => {
   Sentry.init({
-    dsn: 'https://7563ac5ae46e41b1937803ef88c220e8@o366412.ingest.sentry.io/4504146965168128',
+    dsn: 'https://f369ba7ac502474daa212a958791e951@o366412.ingest.sentry.io/4504146888949760',
     tracesSampleRate: 1.0,
   })
 
