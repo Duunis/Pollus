d4b024f734e8723f98ff1e15f2538a6c710881cb
Janne Vanhanen <janne.vanhanen@outlook.com>
2022-11-12 17:56:16 +0200

    Test sentry node

--- functions/sentry.ts
+++ functions/sentry.ts
@@ -1,14 +1,14 @@
-import * as Sentry from '@sentry/browser'
+import * as Sentry from '@sentry/node'
 
 type Context = Parameters<Handler>[0]
 
 export const sentry = (handler: Handler) => (ctx: Context) => {
-  try {
-    Sentry.init({
-      dsn: 'https://f369ba7ac502474daa212a958791e951@o366412.ingest.sentry.io/4504146888949760',
-      tracesSampleRate: 1.0,
-    })
+  Sentry.init({
+    dsn: 'https://7563ac5ae46e41b1937803ef88c220e8@o366412.ingest.sentry.io/4504146965168128',
+    tracesSampleRate: 1.0,
+  })
 
+  try {
     return handler(ctx)
   } catch (error) {
     Sentry.captureException(error)
